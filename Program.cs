d2781a8b48ae4124015f6a1e04b5721fe4736a9a
Renato1614 <renatoaribeiro16@gmail.com>
2018-12-18 13:13:45 -0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var account = new BankAccount("Renato", 1000);
+            Console.WriteLine(account.Balance);
+            account.MakeWithdrawal(600, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "friend paid me back");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
